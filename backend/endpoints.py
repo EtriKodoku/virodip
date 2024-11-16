c6bc016b7406898015eec5cd48549a37186aff4e
mmorekk bc2024-1 <markiian.palyha@gmail.com>
2024-11-16 04:03:19 +0200

    CORS for flask added

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -1,4 +1,10 @@
 from flask import Blueprint, request, jsonify
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Дозволяє всі запити з різних джерел
+
 
 domain = Blueprint('user', __name__)
 

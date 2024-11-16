afc972b2368d78f28157afba31608b14535393f9
bc2024-1 <volodymyrpetriv2207@gmail.com>
2024-11-16 19:58:43 +0200

    Added comment for Marik

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,10 @@ from whiskey import SimpleMiddleware
 
 app = Flask(__name__)
 CORS(app)  # Дозволити CORS для всього додатку
-app.wsgi_app = SimpleMiddleware(app.wsgi_app)  
+
+# Uncomment for prod
+# app.wsgi_app = SimpleMiddleware(app.wsgi_app)  
+
 app.register_blueprint(domain, url_prefix='/api')
 
 if __name__ == '__main__':

fa3256ecf0d14df30ee7d7c80fc4bf0d06ee3fb8
bc2024-1 <volodymyrpetriv2207@gmail.com>
2024-11-22 22:37:16 +0200

    Actually... This is changes from server

--- backend/public_endpoints.py
+++ backend/public_endpoints.py
@@ -1,27 +1,32 @@
 from flask import Blueprint, request, jsonify
-import json
 from models import Users, Activity
 from datetime import datetime
+import pytz
 
 domain = Blueprint("not_user", __name__)
 
 
 @domain.route("/geolocation", methods=["POST"])
 def geolocation():
+    with open("request.txt", "a") as file:
+        file.write(request.get_data().decode('utf-8'))
+        file.close()
     try:
         # Отримання даних з реквесту
-        data = json.loads(request.get_json())
-
-        email = data.get("email")
-        action = data.get("action")
-        time = data.get("time")
+        data = request.get_json()
+        email = data["email"]
+        action = data["action"]
+        time_data = data["time"]
 
         # Перевірка даних
         if not email or action not in ["entered", "exited"] or not time:
             return jsonify({"error": "Invalid input"}), 400
 
         # Перетворення часу в datetime
-        time = datetime.fromisoformat(time)
+        date_format = "%B %d, %Y at %I:%M%p"
+        ua_timezone = pytz.timezone("Europe/Kyiv")
+        time = datetime.strptime(time_data, date_format)
+        time = ua_timezone.localize(time)
 
         # Знаходження або створення користувача
         user, created = Users.get_or_create(email=email)
